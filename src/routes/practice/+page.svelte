9aff6323ded2767b4d5e3e42be7081337becdeb0
Ankur Taxali <ataxali@gmail.com>
2025-11-25 15:04:01 -0500

    feat(practice): add practice page and styling Introduce a new practice page with basic structure and styling. Includes a large button for user interaction.

---
+++ src/routes/practice/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import styles from './page.module.scss';
+</script>
+
+<svelte:head>
+	<title>Practice - Squire</title>
+	<meta name="description" content="Design practice space" />
+</svelte:head>
+
+<div class={styles['practice-page']}>
+    <button class={styles['big-white-button']}>BUTTON</button>
+</div>
